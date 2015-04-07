e988d55154eff3c5bc64bcb4b89f06252e34ff87
Mikel Nelson <github@mikelnelson.net>
2015-04-07 08:58:44 -0700

    ajax

--- media/js/control.js
+++ media/js/control.js
@@ -31,12 +31,16 @@ function inject_data() {
             //$('#id_distroflag').val();
             console.log(json);
             console.log("success");
+
+            pStatus.innerHTML="Finished";
         },
 
         error : function(xhr,errmsg,err) {
             $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
                                             " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
             console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+
+            pStatus.innerHTML="Error";
         }
     });
 };
