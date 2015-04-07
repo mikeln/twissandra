9e9bc389881a2f369665c8ad811901675e9a8467
Mikel Nelson <github@mikelnelson.net>
2015-04-07 10:07:01 -0700

    render conversion

--- inject/views.py
+++ inject/views.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
