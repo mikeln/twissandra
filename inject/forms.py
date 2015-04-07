858fcf0298d257fe33c8437a02230a5b790e3244
Mikel Nelson <github@mikelnelson.net>
2015-04-07 09:29:48 -0700

    fix allowable

--- inject/forms.py
+++ inject/forms.py
@@ -3,8 +3,8 @@ from django import forms
 import cass
 
 class InjectForm(forms.Form):
-    numusers = forms.IntegerField(label='New Users', max_value=100000, min_value=-2)
-    numtweets = forms.IntegerField(label='New Tweets', max_value=100000, min_value=-2)
+    numusers = forms.IntegerField(label='New Users', max_value=100000, min_value=1)
+    numtweets = forms.IntegerField(label='New Tweets', max_value=100000, min_value=1)
     secdelay = forms.IntegerField(label='Delay (sec)', max_value=36400, min_value=0)
     distroflag = forms.BooleanField(label='Random Dist', required=False)
 
