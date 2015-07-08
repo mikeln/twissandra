fc7a67273f3a81ea1ec0b6438281f392ebd4c8a6
Mikel Nelson <github@mikelnelson.net>
2015-07-08 13:49:11 -0700

    change name settings

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ DATABASE_ENGINE = 'django_cassandra_engine'           # 'postgresql_psycopg2', '
 DATABASE_NAME = 'db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = 'cass'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_HOST = 'cassandra.default'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = '9042'             # Set to empty string for default. Not used with sqlite3.
 
 DATABASES = {
