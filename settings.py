9601a3454dad31164a79bb241864c4481c416f14
Mikel Nelson <github@mikelnelson.net>
2019-10-10 16:54:55 -0700

    port is number not str anymore

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ DATABASE_NAME = 'dev.db'             # Or path to database file if using sqlite3
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = 'cassandra.cassandra'             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = '9042'             # Set to empty string for default. Not used with sqlite3.
+DATABASE_PORT = 9042             # Set to empty string for default. Not used with sqlite3.
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
